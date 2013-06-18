ccdfe57798a80e1327c1c6d62758da9d84c6dfef
bogdan.neacsa <bogdan.neacsa@1c0e02f0-7929-43c0-8fb3-3293bf43b0d1>
2013-06-18 12:57:10 +0000

    Changes to tests for 2.7 -> 2.6 linux 32 build revert.
    git-svn-id: https://repo.thevirtualbrain.org/svn/tvb/trunk/framework_tvb@4922 1c0e02f0-7929-43c0-8fb3-3293bf43b0d1

--- tvb_test/analyzers/bct_test.py
+++ tvb_test/analyzers/bct_test.py
@@ -32,7 +32,10 @@
 .. moduleauthor:: Lia Domide <lia.domide@codemart.ro>
 """
 
-import unittest
+try:
+    import unittest2 as unittest
+except Exception:
+    import unittest
 from tvb.core.utils import get_matlab_executable
 from tvb.core.entities import model
 from tvb.core.entities.storage import dao
